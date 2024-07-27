3d7e2abe307f86d32252e9afe135ff75ca954271
amorgun <alexander.morgun.usu@gmail.com>
2024-07-27 15:19:42 +0300

    Vesrion 0.13

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     'description': 'Import and export of Dawn of War models',
     'author': 'amorgun',
     'license': 'GPL',
-    'version': (0, 12),
+    'version': (0, 13),
     'blender': (4, 1, 0),
     'doc_url': 'https://github.com/amorgun/blender_dow',
     'tracker_url': 'https://github.com/amorgun/blender_dow/issues',
